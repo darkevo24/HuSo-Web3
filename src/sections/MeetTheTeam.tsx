c973cde38f51fa6f2852893d2706f4ae87d1b081
darkevo24 <timotius_albert@yahoo.com>
2024-07-04 15:11:34 +0700

    update profile photo

--- src/sections/MeetTheTeam.tsx
+++ src/sections/MeetTheTeam.tsx
@@ -4,21 +4,21 @@ const teamMembers = [
     {
         name: "Huseyin",
         role: "CEO",
-        image: "../../public/assets/huseyin.jpeg",
+        image: "https://media.licdn.com/dms/image/C5603AQGqEaQo99fbXw/profile-displayphoto-shrink_200_200/0/1610134482939?e=1725494400&v=beta&t=Wkuy8mWEwTR1UF0E2NI8xnlhLbItkGZU15wvuLdVoMo",
         description:
             "Huseyin is the visionary behind our company with over 5 years working within different positions in banks and within the blockchain ecosystem.",
     },
     {
         name: "Vish",
         role: "CMO",
-        image: "../../public/assets//vish.jpeg",
+        image: "https://media.licdn.com/dms/image/D4D03AQGTGUv2zxpazA/profile-displayphoto-shrink_200_200/0/1719225218106?e=1725494400&v=beta&t=ILCGVsF2fMgNt36vdkJmrzh1JJQMLvRHerjG_PzsLj0",
         description:
             "Vishaal is a seasoned professional with a diverse background in entrepreneurship and corporate roles, including Consulting at Accenture and PwC, and leadership positions such as COO, CMO, and CPO. He has experience in Esports, AI, Blockchain, and Marketing.",
     },
     {
         name: "Timotius",
         role: "Blockchain Developer",
-        image: "../../public/assets/timotius.png",
+        image: "https://media.licdn.com/dms/image/D5603AQEvQiK3B-BSBA/profile-displayphoto-shrink_200_200/0/1696452416867?e=1725494400&v=beta&t=H4LbIWI1-c1Ftiv9xyMYjBp2VxbsmUiSIFWfkcWy6Uo",
         description:
             "Timotius is our Blockchain developer with a passion for innovation and cutting-edge technology. He has more than 4 years experience in blockchain development and has worked on multiple projects in the blockchain space.",
     },
