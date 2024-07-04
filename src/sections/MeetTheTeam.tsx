450834138fd4396db425d785faaa4d1c95bd2237
darkevo24 <timotius_albert@yahoo.com>
2024-07-05 06:13:52 +0700

    update cmo

--- src/sections/MeetTheTeam.tsx
+++ src/sections/MeetTheTeam.tsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 const teamMembers = [
     {
         name: "Huseyin",
-        role: "CEO",
+        role: "MD",
         image: "https://media.licdn.com/dms/image/C5603AQGqEaQo99fbXw/profile-displayphoto-shrink_200_200/0/1610134482939?e=1725494400&v=beta&t=Wkuy8mWEwTR1UF0E2NI8xnlhLbItkGZU15wvuLdVoMo",
         description:
             "Huseyin is the visionary behind our company with over 5 years working within different positions in banks and within the blockchain ecosystem.",
     },
     {
         name: "Vish",
-        role: "CMO",
+        role: "CTO",
         image: "https://media.licdn.com/dms/image/D4D03AQGTGUv2zxpazA/profile-displayphoto-shrink_200_200/0/1719225218106?e=1725494400&v=beta&t=ILCGVsF2fMgNt36vdkJmrzh1JJQMLvRHerjG_PzsLj0",
         description:
             "Vishaal is a seasoned professional with a diverse background in entrepreneurship and corporate roles, including Consulting at Accenture and PwC, and leadership positions such as COO, CMO, and CPO. He has experience in Esports, AI, Blockchain, and Marketing.",
@@ -21,6 +21,13 @@ const teamMembers = [
         image: "https://media.licdn.com/dms/image/D5603AQEvQiK3B-BSBA/profile-displayphoto-shrink_200_200/0/1696452416867?e=1725494400&v=beta&t=H4LbIWI1-c1Ftiv9xyMYjBp2VxbsmUiSIFWfkcWy6Uo",
         description:
             "Timotius is our Blockchain developer with a passion for innovation and cutting-edge technology. He has more than 4 years experience in blockchain development and has worked on multiple projects in the blockchain space.",
+    },
+    {
+        name: "Stanley",
+        role: "CMO",
+        image: "../../public/assets/CMO.png",
+        description:
+            "I'm stanley, a seasoned expert in marketing and branding with over 6 years of experience in e-commerce. I inject a wide range of marketing strategies to help businesses grow and succeed.",
     },
 
     // Add more team members as needed
