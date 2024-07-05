0189d1470b07472f0af87dfdf54cede69f6b5dd8
darkevo24 <timotius_albert@yahoo.com>
2024-07-05 15:27:41 +0700

    up

--- src/sections/PaymentSection.tsx
+++ src/sections/PaymentSection.tsx
@@ -16,7 +16,7 @@ export default function PaymentSection() {
             });
     };
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center text-center">
             <h3 className="text-5xl">BTC Payment</h3>
             <p className="my-4">Scan the QR code below to pay</p>
             <img src="/assets/btc-address.jpg" alt="btc" className="w-60" />
