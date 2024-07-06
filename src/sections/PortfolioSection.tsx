9fb23e5127d08ea515466e6564458cdd742c51c4
darkevo24 <timotius_albert@yahoo.com>
2024-07-06 15:01:14 +0700

    up

--- src/sections/PortfolioSection.tsx
+++ src/sections/PortfolioSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PortfolioSection() {
     return (
-        <div className="flex items-center justify-center lg:space-x-4 lg;space-y-0 space-y-4 flex-col lg:flex-row">
+        <div className="flex items-center justify-center lg:space-x-4 lg:space-y-0 space-y-4 flex-col lg:flex-row">
             <img
                 className="lg:w-3/12 w-10/12 w-full rounded-lg"
                 src="/assets/photo3.jpg"
